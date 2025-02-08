8aa01525a766a0fdf314572e1c6f8c2cb13cae33
Isaac Makinde <ismakinde@gmail.com>
2025-02-08 22:09:12 +0100

    Added action to form in CreateOrder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -203,3 +203,32 @@ const [withPriority, setWithPriority] = useState(false);
   );
 }
 
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'true',
+  };
+
+  console.log(order);
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+  const newOrder = await createOrder(order);
+
+  // Do NOT overuse
+  store.dispatch(clearCart());
+
+  return redirect(`/order/${newOrder.id}`);
+}
+
+export default CreateOrder;
