147823c76aa74db3ea84942c84afd3a8d5704d54
Oluwa-Laughter <ismakinde@gmail.com>
2025-02-18 22:54:07 +0100

    update the add to cart functionality and also  EmptyCart display component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -5,6 +5,8 @@ import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart } from "./cartSlice";
 
+import EmptyCart from "./EmptyCart";
+
 const fakeCart = [
   {
     pizzaId: 12,
@@ -36,10 +38,10 @@ function Cart() {
 
   const dispatch = useDispatch();
 
+  if (!cart.length) return <EmptyCart />;
+
   return (
     <div className="px-4 py-3">
-      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
-
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
