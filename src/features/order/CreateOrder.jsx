63b53bc7b27c23a671370813d415ab846e6a57fe
Oluwa-Laughter <ismakinde@gmail.com>
2025-02-21 23:44:24 +0100

    update order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -139,24 +139,3 @@ export async function action({ request }) {
 }
 
 export default CreateOrder;
-
-
-
-// import { formatCurrency } from "../../utils/helpers";
-
-// function OrderItem({ item, isLoadingIngredients, ingredients }) {
-//   const { quantity, name, totalPrice } = item;
-
-//   return (
-//     <li className="py-3">
-//       <div className="flex items-center justify-between gap-4 text-sm">
-//         <p>
-//           <span className="font-bold">{quantity}&times;</span> {name}
-//         </p>
-//         <p className="font-bold">{formatCurrency(totalPrice)}</p>
-//       </div>
-//     </li>
-//   );
-// }
-
-// export OrderItem;
