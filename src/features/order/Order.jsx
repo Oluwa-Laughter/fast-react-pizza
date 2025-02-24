b6f3eccae0fb69a04b9328b59b9a00bc88b688f7
Oluwa-Laughter <ismakinde@gmail.com>
2025-02-24 21:10:01 +0100

    fix key prop for the cartItem

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -51,7 +51,7 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-t border-b">
         {cart.map((item) => (
-          <OrderItem key={item.id} item={item} />
+          <OrderItem key={item.pizzaId} item={item} />
         ))}
       </ul>
 
